604373c4e39bb3f966f2fa8cba67e52c043aaf71
Uy Nguyen <uyhdnguyen@gmail.com>
2024-09-08 07:57:37 -0400

    Update add todo to existing todo list in todo project

--- todo.rb
+++ todo.rb
@@ -4,11 +4,36 @@ require 'securerandom'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'sinatra/content_for'
 
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
 end
+
+=begin  
+HELPER METHODS
+=end
+
+# Return an error message if the list name is invalid. Return nil if name is valid
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
+# Return an error message if the list name is invalid. Return nil if name is valid
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    'Todo must be between 1 and 100 characters.'
+  end
+end
+
+=begin  
+################################################################################################
+=end
 
 before do
   session[:lists] ||= []
@@ -24,25 +49,16 @@ get '/lists' do
   erb :lists, layout: :layout
 end
 
-# Render the new list form
+# 1.0 Render the new list form (Create a form that sends POST request)
 get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
-# Return an error message if the list name is invalid. Return nil if name is valid
-def error_for_list_name(name)
-  if !(1..100).cover? name.size
-    'List name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name] == name }
-    'List name must be unique.'
-  end
-end
-
-# Create a new list
+# 1.1 Create a POST request to create  new list
 post '/lists' do
   list_name = params[:list_name].strip
-  error = error_for_list_name(list_name)
 
+  error = error_for_list_name(list_name)
   if error
     session[:error] = error
     erb :new_list, layout: :layout
@@ -50,5 +66,61 @@ post '/lists' do
     session[:lists] <<  { name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
+  end
+end
+
+# 2.1 Render a single todo list
+get '/lists/:id' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :list, layout: :layout
+end
+
+# 3.1 Render the edit form an existing todo list (Create a form that sends POST request)
+get '/lists/:id/edit' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :edit_list, layout: :layout
+end
+
+# 3.2 Create a POST request to edit the existing todo list
+post '/lists/:id' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{:@list_id}"
+  end
+end
+
+# 3.3 Delete an existing todo list
+post '/lists/:id/destroy' do
+  @list_id = params[:id].to_i
+  session[:lists].delete_at @list_id
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+end
+
+# Add new todo to an existing list
+post '/lists/:list_id/todos' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: text, completed: false}
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
   end
 end
